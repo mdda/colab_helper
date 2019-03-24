f55e74b03c43de760be539d8e0c82f4846fe3289
Martin Andrews <GitHub@mdda.net>
2019-03-24 20:16:05 +0800

    Minor niggle with gdrive

--- utils.py
+++ utils.py
@@ -1,2 +1,13 @@
+import os
+
 def status():
   print("Doing fine")
+  
+def gdrive_mount(point='gdrive', link='my_drive'):
+  from google.colab import drive
+  drive.mount(point)
+  if link is not None:
+    import subprocess
+    # ! ln -s "gdrive/My Drive" my_drive 
+    subprocess.run(["ln", "-s", point+"/My Drive", link,])
+    print("'%s' mounted as '%s'" % (point+"/My Drive", link,))
