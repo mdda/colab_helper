151d3a27484a7f812c0f402c9091db2f249ba16d
Martin Andrews <GitHub@mdda.net>
2019-04-24 22:11:58 +0800

    Logo output - attempt 8

--- utils.py
+++ utils.py
@@ -16,9 +16,10 @@ def status():
   img_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), 'img', 'RedDragon_logo_260x39.png')
   #img_path = './colab_helper/img/RedDragon_logo_260x39.png'
   print(img_path)
-  pil_im = Image.open(img_path) #Take jpg + png
-  plt.imshow(np.asarray(pil_im))
-  plt.show()
+  
+  #pil_im = Image.open(img_path) #Take jpg + png
+  #plt.imshow(np.asarray(pil_im))
+  #plt.show()
   
   return
   
